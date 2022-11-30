b81b5361a55469aa99993fa29af9f093952563aa
Mirko Franco <francomirko91@gmail.com>
2022-11-30 15:00:32 +0100

    defined the instance variables to the object

--- src/firstExercise/Programmer.java
+++ src/firstExercise/Programmer.java
@@ -4,4 +4,14 @@ public class Programmer {
     public String name;
     public int age;
     public boolean wearsGlasses;
+
+    void drinkCoffe(){
+        System.out.println("Espresso is the secret!");
+    }
+    void printDetails(){
+        System.out.println(name + "is a " + age + "-yo programmer");
+    }
+    void hasGlasses(){
+        System.out.println("Is " + name + " wearing glasses?");
+    }
 }
--- src/firstExercise/Start.java
+++ src/firstExercise/Start.java
@@ -2,6 +2,26 @@ package firstExercise;
 
 public class Start {
     public static void main(String[] args) {
+     Programmer p1 = new Programmer();
+
+     Programmer p2 = new Programmer();
+
+     p1.name = "Mario";
+     p1.age = 25;
+     p1.wearsGlasses = true;
+
+     p2.name = "Luca";
+     p2.age = 30;
+     p2.wearsGlasses = false;
+
+
+
+
+
+
+
+
+
 
     }
 }
